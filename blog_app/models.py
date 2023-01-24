331c7ae5c0daa2cc4925946120c4d9c1a17bda4d
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-24 09:03:07 +0300

    post model

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# Post model
+class Post(models.Model):
+    title = models.CharField(max_length=250) # title of the post, VARCHAR
+    slug = models.SlugField(max_length=250) # short lable, VARCHAR
+    body = models.TextField() # body, TEXT
+
+    def __str__(self): # return human readable representation of the object
+        return self.title
