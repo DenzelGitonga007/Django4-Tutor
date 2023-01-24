c565f872975fd976a0fe8c1d5ae4bb0dc6752d0d
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-24 09:09:23 +0300

    datetime fields

--- blog_app/models.py
+++ blog_app/models.py
@@ -6,6 +6,12 @@ class Post(models.Model):
     title = models.CharField(max_length=250) # title of the post, VARCHAR
     slug = models.SlugField(max_length=250) # short lable, VARCHAR
     body = models.TextField() # body, TEXT
+    # Datetime fields.
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+
 
     def __str__(self): # return human readable representation of the object
         return self.title
