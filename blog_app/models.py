4c99d7cc3c4f4ed3d09a3200a26ffa6098c4bf17
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-24 09:30:13 +0300

    status of the post

--- blog_app/models.py
+++ blog_app/models.py
@@ -4,6 +4,11 @@ from django.utils import timezone
 # Create your models here.
 # Post model
 class Post(models.Model):
+    # Add status field
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=250) # title of the post, VARCHAR
     slug = models.SlugField(max_length=250) # short lable, VARCHAR
     body = models.TextField() # body, TEXT
@@ -11,6 +16,8 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    # Status
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
     # Defining the sort order
     class Meta:
         ordering = ['-publish'] # order by the publishing field
