5c440cc8b4f1eb6d766660ccb4d7ae8ef9b11fd7
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-26 08:59:33 +0300

    advanced customization

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -9,3 +9,10 @@ from .models import Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'author', 'publish', 'status']
+    # More customization
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)} #autocomplete slug field
+    raw_id_fields = ['author'] # to search for users
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
