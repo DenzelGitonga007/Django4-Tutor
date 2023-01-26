f475da85d5ca778eef1c1d4e004e3b3ec4939d5c
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-26 08:47:17 +0300

    customize the admin view

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+# admin.site.register(Post)
+
+# Customize the admin site
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
